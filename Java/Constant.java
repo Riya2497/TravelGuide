8063615f07f6983312fb61640552c7cbeaf49c3f
sakshipink <44083326+sakshipink@users.noreply.github.com>
2020-09-05 16:06:00 -0400

    Add files via upload

---
+++ Java/Constant.java
@@ -0,0 +1,22 @@
+package com.jignesh.streety;
+
+public class Constant {
+
+    public static final String BASE_URL = "http://192.168.0.6:8080/";
+    // TODO: Note: Don't create urls as BASE_URL + "some_url"
+
+    // Add all your urls here...
+    public static final String URL_LOGIN = "streety/login";
+    public static final String URL_REGISTER = "streety/register";
+    public static final String URL_FORGOTPWD = "streety/fPassC";
+    public static final String URL_INSERT_TRIPS="streety/inserttripdetailC";
+    public static final String URL_LIST_PLACES="streety/detailsC";
+    public static final String URL_COUNT_TRIPS="streety/countTripC";
+    public static final String URL_VIEW_TRIPS="streety/viewtripsC";
+    public static final String URL_ALL_PLACES="streety/listAllC";
+    public static final String URL_FB_LOGIN="streety/facebookloginC";
+    public static final String URL_GOOGLE_LOGIN="streety/googleloginC";
+
+
+}
+
